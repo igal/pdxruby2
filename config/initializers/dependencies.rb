17310ab0e6cbe6f1261fa8bcfc5c98a03807274a
Igal Koshevoy <igal@pragmaticraft.com>
2009-08-19 09:29:36 -0700

    Added facets.

--- config/initializers/dependencies.rb
+++ config/initializers/dependencies.rb
@@ -1,2 +1,11 @@
 # Standard libraries
 require 'ostruct'
+
+# Facets
+require 'facets/dictionary'
+require 'facets/enumerable/mash'
+require 'facets/file/write'
+require 'facets/kernel/ergo'
+require 'facets/kernel/with'
+require 'facets/boolean'
+require 'facets/string/word_wrap'
