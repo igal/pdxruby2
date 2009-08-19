c9d9ddb49ec352e5f49daf51983bd087b8149897
Igal Koshevoy <igal@pragmaticraft.com>
2009-08-19 11:37:49 -0700

    Reimplemented events#index.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.xml
   def index
-    @events = Event.all
+    @events = Event.paginate(:page => params[:page])
 
     respond_to do |format|
       format.html # index.html.erb
