e53be87d375949141504036e0b2cd43189d9b3cb
Igal Koshevoy <igal@pragmaticraft.com>
2009-08-20 06:58:19 -0700

    Wrote basic HomeController spec, although can't get changes to render within a test.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,10 +1,34 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe HomeController do
+  fixtures :members, :locations, :events
+  describe "GET index" do
+    it "should display page and events" do
+      get :index
 
-  #Delete this example and add some real ones
-  it "should use HomeController" do
-    controller.should be_an_instance_of(HomeController)
+      events = assigns[:events]
+      events.include?(events(:meeting))
+      events.include?(events(:meeting2))
+    end
+  end
+
+  describe "GET changes" do
+    before(:each) do
+      @location = Location.create!(:name => "A place", :address => "An address")
+      @location.name = "Renamed place"
+      @location.save
+    end
+
+    it "should display changes" do
+      get :changes, :format => "html"
+      pending "Figure out why /changes isn't rendering in tests"
+    end
+
+    it "should display changes feed" do
+      get :changes, :format => "atom"
+      pending "Figure out why /changes isn't rendering in tests"
+    end
+
   end
 
 end
