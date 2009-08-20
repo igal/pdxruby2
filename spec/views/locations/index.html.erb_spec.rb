bc3beb53c5c6bca6ef46480ed05b567132d9790e
Igal Koshevoy <igal@pragmaticraft.com>
2009-08-20 06:26:05 -0700

    Wrote location view specs.

--- spec/views/locations/index.html.erb_spec.rb
+++ spec/views/locations/index.html.erb_spec.rb
@@ -3,25 +3,16 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 describe "/locations/index.html.erb" do
   include LocationsHelper
 
+  fixtures :members, :locations, :events
+
   before(:each) do
-    assigns[:locations] = [
-      stub_model(Location,
-        :name => "value for name",
-        :address => "value for address",
-        :homepage => "value for homepage"
-      ),
-      stub_model(Location,
-        :name => "value for name",
-        :address => "value for address",
-        :homepage => "value for homepage"
-      )
-    ]
+    @cubespace = locations(:cubespace)
+    assigns[:locations] = [ @cubespace ]
   end
 
   it "renders a list of locations" do
     render
-    response.should have_tag("tr>td", "value for name".to_s, 2)
-    response.should have_tag("tr>td", "value for address".to_s, 2)
-    response.should have_tag("tr>td", "value for homepage".to_s, 2)
+    response.should have_text /#{@cubespace.name}/
+    response.should have_text /#{@cubespace.address}/
   end
 end
