c082feba2883c580966b564cb2f8414afecda1c8
Igal Koshevoy <igal@pragmaticraft.com>
2009-08-20 17:14:14 -0700

    Reworked settings initializer to use sample data when running in non-production mode, to make it easier to get started.

--- config/initializers/00_settings.rb
+++ config/initializers/00_settings.rb
@@ -2,18 +2,26 @@
 
 require 'settings_reader'
 
-filename_relative = "config/settings.yml"
-filename_complete = "#{RAILS_ROOT}/#{filename_relative}"
+filename_relative_real = "config/settings.yml"
+filename_absolute_real = "#{RAILS_ROOT}/#{filename_relative_real}"
+filename_relative_sample = "#{filename_relative_real}.sample"
+filename_absolute_sample = "#{RAILS_ROOT}/#{filename_relative_sample}"
 
-if File.exist?(filename_complete)
-  SETTINGS = SettingsReader.read(filename_complete)
+if File.exist?(filename_absolute_real)
+  #IK# puts "* Loading custom SETTINGS from: #{filename_relative_sample}"
+  SETTINGS = SettingsReader.read(filename_absolute_real)
 else
-  puts <<-TEXT
+  if RAILS_ENV == "production"
+    puts <<-TEXT
 +---[ ERROR ]-----------------------------------------------------------
-| You must create a "#{filename_relative}" file to store your settings.
-| You should make this by copying "#{filename_relative}.sample",
+| You MUST create a "#{filename_relative_real}" file to store your settings.
+| You should make this by copying "#{filename_relative_sample}",
 | which provides further instructions on what to put in this file.
 +-----------------------------------------------------------------------
-  TEXT
-  raise IOError, "Couldn't read settings from: #{filename_relative}"
+    TEXT
+    raise IOError, "Couldn't read settings from: #{filename_relative_real}"
+  else
+    puts "* Loading sample SETTINGS from: #{filename_relative_sample}"
+    SETTINGS = SettingsReader.read(filename_absolute_sample)
+  end
 end
