e8dd6ce492d66410d4c564d443f7a97d7489a7c7
Igal Koshevoy <igal@pragmaticraft.com>
2009-12-28 15:50:36 -0800

    Improved members#index and members#show view specs to check email encoding.

--- spec/views/members/index.html.erb_spec.rb
+++ spec/views/members/index.html.erb_spec.rb
@@ -9,12 +9,31 @@ describe "/members/index.html.erb" do
     assigns[:members] = @members = [ members(:aaron), members(:bubba), members(:clio) ]
   end
 
-  it "renders a list of members" do
+  it "renders a list of members for anonymous user" do
+    logout
+
     render
     response.should have_text /#{@members[0].name}/
     response.should have_text /#{@members[1].name}/
     response.should have_text /#{@members[2].name}/
 
     response.should have_text /#{member_path(@members[0])}/
+
+    response.should_not have_text /Email/
+    response.should_not have_text /decodeURIComponent/
+  end
+
+  it "renders a list of members with emails for logged-in user" do
+    login_as :bubba
+
+    render
+    response.should have_text /#{@members[0].name}/
+    response.should have_text /#{@members[1].name}/
+    response.should have_text /#{@members[2].name}/
+
+    response.should have_text /#{member_path(@members[0])}/
+
+    response.should have_text /Email/
+    response.should have_text /decodeURIComponent/
   end
 end
--- spec/views/members/show.html.erb_spec.rb
+++ spec/views/members/show.html.erb_spec.rb
@@ -2,16 +2,32 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/members/show.html.erb" do
   include MembersHelper
-  
+
   fixtures :members
 
   before(:each) do
     assigns[:member] = @member = members(:aaron)
   end
 
-  it "renders attributes in <p>" do
+  it "renders attributes for anonymous user" do
+    logout
+
     render
     response.should have_text /#{@member.name}/
     response.should have_text /#{@member.about}/
+
+    response.should_not have_text /Email/
+    response.should_not have_text /decodeURIComponent/
+  end
+
+  it "renders attributes for logged-in user" do
+    login_as :bubba
+
+    render
+    response.should have_text /#{@member.name}/
+    response.should have_text /#{@member.about}/
+
+    response.should have_text /Email/
+    response.should have_text /decodeURIComponent/
   end
 end
