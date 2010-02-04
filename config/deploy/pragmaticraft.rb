f011e1105387e6fbe2a2baea21af715e11d0d144
Igal Koshevoy <igal@pragmaticraft.com>
2010-02-04 04:44:20 -0800

    Switched to :copy deploy to avoid depending on github for deploying releases.

--- config/deploy/pragmaticraft.rb
+++ config/deploy/pragmaticraft.rb
@@ -1,11 +1,20 @@
-set :scm, "git"
-set :repository,  "git://github.com/igal/pdxruby2.git"
-set :branch, "master"
+#set :scm, "git"
+#set :repository,  "git://github.com/igal/pdxruby2.git"
+#set :branch, "master"
+#set :deploy_via, :remote_cache
+
+set :scm, :git
+set :git_shallow_copy, 1
+set :repository, '.'
+set :deploy_via, :copy
+set :copy_cache, true
+set :copy_exclude, ['.git', 'log', 'tmp', '*.sql', '*.diff', 'coverage.info', 'coverage', 'public/images/members', 'public/system', 'tags', 'db/remote.sql', 'db/*.sqlite3', '*.swp', '.*.swp']
+
 set :deploy_to, "/var/www/pdxruby"
 set :host, "dev.pragmaticraft.com"
 set :user, "pdxruby"
 
-set :deploy_via, :remote_cache
+
 role :app, host
 role :web, host
 role :db,  host, :primary => true
