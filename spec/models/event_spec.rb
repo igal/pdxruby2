d61ef0a31b25a0af819d3d489334180a05b4992a
Igal Koshevoy <igal@pragmaticraft.com>
2010-04-27 20:14:53 -0700

    Improved event_spec, added examples for cloning events, cloning times, and iCalendar locations.

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -58,6 +58,7 @@ describe Event do
       component.summary.should == item.name
       component.description.should == item.agenda
       component.url.should == url_helper.call(item) if url_helper
+      component.location.should == [item.location.name, item.location.address].join(', ') if item.location
     end
 
     # Return the calendar component matching the given item's name.
@@ -79,7 +80,8 @@ describe Event do
           :name => "Meeting 1",
           :agenda => "Agenda 1",
           :starts_at => 1.day.from_now,
-          :ends_at => 1.day.from_now + 1.hour),
+          :ends_at => 1.day.from_now + 1.hour,
+          :location => location),
         stub_model(Event,
           :id => 2,
           :name => "Meeting 2",
@@ -97,6 +99,72 @@ describe Event do
 
       find_and_assert_calendar_match(events[0], components, url_helper)
       find_and_assert_calendar_match(events[1], components, url_helper)
+    end
+  end
+
+  describe "when cloning" do
+    describe "cloning from an event" do
+      it "should return a cloned event" do
+        time_of_day = 6.hours + 5.minutes + 4.seconds
+        source_time = Time.parse('2009/8/7') + time_of_day
+        source_duration = 2.hours
+        attributes = {
+          :location_id => 1,
+          :member_id => 2,
+          :name => "IMPORTANT EVENT!",
+          :status => "omgkittens",
+          :agenda => "The same thing we do every night",
+          :starts_at => source_time,
+          :ends_at => source_time + source_duration,
+          :minutes => "Yay!",
+          :created_at => Time.now - 1.day
+        }
+        source = stub_model(Event, attributes)
+        
+        result = Event.clone_from(source)
+        result.starts_at.should == Date.today.to_time + time_of_day
+        result.ends_at.should == Date.today.to_time + time_of_day + source_duration
+        result.name.should == source.name
+        result.member_id.should == source.member_id
+        result.location_id.should == source.location_id
+        result.agenda.should == source.agenda
+        result.minutes.should be_blank
+      end
+    end
+
+    describe "cloning a time" do
+      it "should return date for today, but time-of-day from given event" do
+        time_of_day = 6.hours + 5.minutes + 4.seconds
+        source = Time.parse('2009/8/7') + time_of_day
+
+        result = Event._clone_time_for_today(source)
+        result.should == Date.today.to_time + time_of_day
+      end
+    end
+  end
+
+  describe "can_alter?" do
+    before do
+      @admin  = stub_model(Member, :admin => true)
+      @owner  = stub_model(Member)
+      @member = stub_model(Member)
+      @event  = stub_model(Event, :member => @owner)
+    end
+
+    it "should be for admin" do
+      @event.can_alter?(@admin).should be_true
+    end
+
+    it "should be for owner" do
+      @event.can_alter?(@owner).should be_true
+    end
+
+    it "should not be for non-owner non-admin" do
+      @event.can_alter?(@member).should be_false
+    end
+
+    it "should not be for anonymous" do
+      @event.can_alter?(nil).should be_false
     end
   end
 end
