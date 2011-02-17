7572c29fb9dedba947a69650de2da4d3e60f9e37
Igal Koshevoy <igal@pragmaticraft.com>
2011-02-17 00:54:12 -0800

    Improved members#_form to show required fields and explain attributes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def required_field
+    return content_tag(:span, '(Required)', :class => [:required_field])
+  end
 end
