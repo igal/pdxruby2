40728f9380b21b20cd2e21858a7090c7de7af436
Igal Koshevoy <igal@pragmaticraft.com>
2011-03-24 13:48:29 -0700

    Improve styling of required fields, replaced #required_field helper with CSS.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,3 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def required_field
-    return content_tag(:span, '(Required)', :class => [:required_field])
-  end
 end
